855efcb0a726aed0125d6de654897cfe986a52fa
Michael Katsevman <mikek@hlcam.com>
2012-10-23 11:22:51 -0700

    Made the transparent redis wrapping safe and much more elegant

--- kvnest.py
+++ kvnest.py
@@ -2,7 +2,10 @@ from functools import partial
 import inspect
 
 class KVNest(str):
+	"""A subclass of str that allows namespacing keys for use in redis as well as provide partial methods into redis."""
+
 	def __new__(cls,*args,**kwargs):
+		"""Override's str's __new__ so we can store the redis connection if one is provided"""
 		if 'connection' in kwargs:
 			connection = kwargs['connection']
 			del kwargs['connection']
@@ -14,19 +17,13 @@ class KVNest(str):
 		return obj
 		
 	def __getitem__(self,key):
+		"""Concatenates the key and returns a new KVNest instead of indexing into the string"""
 		return KVNest(':'.join((self,key)))
 
 	def __getattribute__(self,name):
 		"""Transparently returns partially applied versions of redis methods."""
-		
-		# if we're not looking for the redis connection and we do have a connection, look to see if this exists within redis
-		if name!='_redis' and hasattr(self,'_redis') and hasattr(self._redis, name): 
-			
-			redis_attr = getattr(self._redis,name)
-
-			if inspect.ismethod(redis_attr) and 'redis' in redis_attr.im_class.__name__.lower():
-				return partial(redis_attr,str(self))
-			else:
-				return super(KVNest,self).__getattribute__(name)
-		else:
+		try:
 			return super(KVNest,self).__getattribute__(name)
+		except AttributeError, e:
+			return partial(getattr(self._redis,name),str(self))
+			# return super(KVNest,self).__getattribute__(name)
